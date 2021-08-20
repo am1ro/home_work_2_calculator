888b8b6ee72807d2822a55b637011ea4f0e8c2df
levinmac <am1ro1@bk.ru>
2021-08-20 20:48:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/home_work_2_calculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.home_work_2_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
